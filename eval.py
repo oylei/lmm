58ab7305af3881693ef289bf7e5e5c5c26715586
oylei <OUyang_lei0424@163.com>
2026-07-07 14:28:43 +0800

    lora

--- eval.py
+++ eval.py
@@ -5,7 +5,7 @@ import warnings
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, TextStreamer
 from model.MiniMindModel import MiniMindConfig, MiniMindForCausalLM
-# from model.model_lora import *
+from model.model_lora import *
 from trainer.trainer_utils import setup_seed, get_model_params
 warnings.filterwarnings('ignore')
 
